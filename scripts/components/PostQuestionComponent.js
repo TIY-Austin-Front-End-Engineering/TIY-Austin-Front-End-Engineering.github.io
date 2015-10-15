1a53f9704bf0394c6db520ead5fa5c3fc2eb149c
Aaron Larner <anlarner@gmail.com>
2015-10-15 14:23:51 -0500

    New deploy

--- scripts/components/PostQuestionComponent.js
+++ scripts/components/PostQuestionComponent.js
@@ -31,7 +31,6 @@ module.exports = React.createClass({
 	    );
 	},
 	render: function() {
-		console.log('render '+ this.state.errorElement)
 	//once a new multiple choice answer is added in, choiceRows will map and display onto the page
 		var choiceRows = this.state.choices.map(function(choice){
 			return(
@@ -68,7 +67,7 @@ module.exports = React.createClass({
 					{choiceRows}
 					</div>
 					{this.state.feedbackElement}
-				<button onClick={this.onSubmit}>Submit Question</button>
+				<button ref="button" disabled={false} onClick={this.onSubmit}>Submit Question</button>
 			</div>
 		</div>
 		);
@@ -84,8 +83,8 @@ module.exports = React.createClass({
 			}
 		}
 		//once question is filled out, send to the server
-		if(correctAnswer === null){
-			this.setState({feedbackElement: 'this is an error'});
+		if(correctAnswer === null || this.refs.questionTitle.value === ''){
+			this.setState({feedbackElement: 'Please fill in all fields and select a correct answer'});
 		}else{
 			var quizId =this.props.quizId;
 			var targetQuizModel = new QuizModel({objectId: quizId});
@@ -96,11 +95,13 @@ module.exports = React.createClass({
 				correctChoice: correctAnswer
 				
 			});
+			targetQuizModel.increment('totalQuestions');
+			targetQuizModel.save();
 			newQuestion.save();
+			this.refs.button.disabled = true;
 			this.refs.questionTitle.value = '',
-			this.refs.choice.value = '',
 			this.setState({choices: []});
-			this.setState({feedbackElement: 'new question submitted'});
+			this.setState({feedbackElement: 'New question submitted'});
 			this.props.router.navigate('editQuiz/'+this.state.quiz.id, {trigger: true});
 		}
 		
@@ -109,9 +110,14 @@ module.exports = React.createClass({
 	},
 	onAddChoice: function(){
 	//push the multiple choice answers to the choice array
-		var newChoice = this.refs.choice.value;
-		var currentChoices = this.state.choices;
-		currentChoices.push(newChoice);
-		this.setState({choices: currentChoices})	
+		if(this.refs.choice.value === ''){
+			this.setState({feedbackElement: 'Please fill in an answer'});
+		}else{
+			var newChoice = this.refs.choice.value;
+			var currentChoices = this.state.choices;
+			currentChoices.push(newChoice);
+			this.setState({choices: currentChoices}),
+			this.refs.choice.value = '';
+		}	
 	}
 });
